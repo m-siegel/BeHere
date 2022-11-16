123b70c508e9f16aa8a8331960228ac94f2b66db
m-siegel <mea.siegel@gmail.com>
2022-11-15 22:36:20 -0800

    added HomePage back

---
+++ front-end/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+// Mea
+
+import BasePage from "../components/base-page-components/BasePage.js";
+
+function HomePage() {
+  return (
+    <BasePage>
+      <h1>Home Page</h1>
+      <p>Some paragrap text</p>
+    </BasePage>
+  );
+}
+
+export default HomePage;

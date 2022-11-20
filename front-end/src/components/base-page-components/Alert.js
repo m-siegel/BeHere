d634b844088c921406c31dc4f27d7f3d69f911a7
m-siegel <mea.siegel@gmail.com>
2022-11-20 13:56:33 -0800

    draft alert component

--- front-end/src/components/base-page-components/Alert.js
+++ front-end/src/components/base-page-components/Alert.js
@@ -1,10 +1,31 @@
 // Tim C
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-function Alert() {
+function Alert(type, heading, message) {
+  // TODO: validate type is "warning", "success", etc.
   return (
-    <div>Alert</div>
-  )
+    <div className="Alert">
+      <div
+        class={`alert alert-${type} alert-dismissible fade show`}
+        role="alert"
+      >
+        <strong>{heading}</strong> {message}
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
+    </div>
+  );
 }
 
-export default Alert
+Alert.propTypes = {
+  type: PropTypes.string.isRequired,
+  heading: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+export default Alert;

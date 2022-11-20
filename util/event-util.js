66028da3747588107fc344c7961de98c2c03da1e
tcrawley2 <62876986+tcrawley2@users.noreply.github.com>
2022-11-19 17:59:08 -0800

    committing event-util

--- util/event-util.js
+++ util/event-util.js
@@ -1,23 +1,23 @@
 const eventUtil = {};
 export function eventify(obj) {
-    const object = {
-        name: obj.name,
-        description: obj.description,
-        eventOrgName: obj.eventOrgName,
-        creator: obj.creator,
-        // String
-        location: obj.location,
-        // Google API to give us info about local time zone?
-        // Saving start & finish as a string
-        start: obj.start,
-        finish: obj.finish,
-        tags: [],
-        followedBy: [],
-        rsvpYes: [],
-        rsvpMaybe: [],
-        rsvpNo: []
-    }
-    return object;
+  const object = {
+    name: obj.name,
+    description: obj.description,
+    organization: obj.organization,
+    creator: obj.creator,
+    // String
+    location: obj.location,
+    // Google API to give us info about local time zone?
+    // Saving start & finish as a string
+    start: obj.start,
+    finish: obj.finish,
+    tags: [],
+    followedBy: [],
+    rsvpYes: [],
+    rsvpMaybe: [],
+    rsvpNo: [],
+  };
+  return object;
 }
 eventUtil.eventify = eventify;
 

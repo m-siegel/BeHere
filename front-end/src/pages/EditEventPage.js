1406434063fd2f74e42da491202330469ec689c3
tcrawley2 <62876986+tcrawley2@users.noreply.github.com>
2022-11-19 18:00:16 -0800

    edit event page (possible bug)

--- front-end/src/pages/EditEventPage.js
+++ front-end/src/pages/EditEventPage.js
@@ -1,14 +1,24 @@
 // By Tim
-
+import React from "react";
 import BasePage from "../components/base-page-components/BasePage.js";
+import EventForm from "../components/EventForm.js";
+import PropTypes from "prop-types";
 
-function EditEventPage() {
+function EditEventPage({ event }) {
   return (
     <BasePage>
-      <h1>Edit Event</h1>
-      <p>Some paragrap text</p>
+      <div class="card">
+        <div class="card-body">
+          <h1>Edit your event</h1>
+          <EventForm />
+        </div>
+      </div>
     </BasePage>
   );
 }
+
+EditEventPage.propTypes = {
+  event: PropTypes.object,
+};
 
 export default EditEventPage;

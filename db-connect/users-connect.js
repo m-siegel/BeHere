a9c8a39942a8da11b59e3bb39517937e79488eb5
tcrawley2 <62876986+tcrawley2@users.noreply.github.com>
2022-11-20 14:05:17 -0800

    initializing env in users db file

--- db-connect/users-connect.js
+++ db-connect/users-connect.js
@@ -1,13 +1,18 @@
 /** By Ilana-Mahmea */
 
 import * as mongodb from "mongodb";
-const uri = process.env.MONGO_URI || "mongodb://localhost:27017";
+let uri = process.env.MONGO_URI || "mongodb://localhost:27017";
 const userConnect = {}; // To export default
 
 const databaseName = "be-here-db";
 userConnect.databaseName = databaseName;
 const usersCollectionName = "users";
 userConnect.collectionName = usersCollectionName;
+
+export function initializeURI() {
+  uri = process.env.MONGO_URI || "monogodb://localhost:27017";
+}
+userConnect.initializeURI = initializeURI;
 
 // Generic functions
 

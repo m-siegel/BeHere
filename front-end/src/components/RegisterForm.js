bd2cf13b7b78fa559e8f46b089bb7bba359e78b4
m-siegel <mea.siegel@gmail.com>
2022-11-20 23:38:42 -0800

    working registration page and form

--- front-end/src/components/RegisterForm.js
+++ front-end/src/components/RegisterForm.js
@@ -1,6 +1,7 @@
-import React from "react";
-// import PropTypes from "prop-types";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import FormInput from "./FormInput";
+import { Link } from "react-router-dom";
 
 // TODO: validation of password and password confirmation? validation of email?
 
@@ -8,26 +9,135 @@ import FormInput from "./FormInput";
 // on update state variables on every change, then they're there
 // when submit is clicked
 
-function RegisterForm() {
+function RegisterForm({ setAlert }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [orgEmail, setOrgEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function onSubmit(evt) {
+    evt.preventDefault();
+    setAlert({
+      type: "",
+      heading: "",
+      message: <div>Attempting registration...</div>,
+    });
+
+    const res = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        orgEmail: orgEmail,
+        contactEmail: orgEmail,
+        password: password,
+      }),
+    });
+    const resJSON = await res.json();
+
+    alertUser(resJSON);
+    if (resJSON.success) {
+      clearForm();
+    }
+  }
+
+  function alertUser(info) {
+    if (info.success) {
+      setAlert({
+        type: "success",
+        heading: "Registration successful!",
+        message: (
+          <div>
+            Head over to <Link to="/login">login</Link>
+          </div>
+        ),
+      });
+    } else {
+      switch (info.reason) {
+        case "database":
+          setAlert({
+            type: "danger",
+            heading: "Uh oh.",
+            message: (
+              <div>
+                <p>We encountered a problem with our database.</p>
+                <p>Please try again later.</p>
+              </div>
+            ),
+          });
+          break;
+        case "route":
+          setAlert({
+            type: "danger",
+            heading: "Uh oh.",
+            message: (
+              <div>
+                <p>We encountered a problem with our database.</p>
+                <p>Please try again later.</p>
+              </div>
+            ),
+          });
+          break;
+        default:
+          setAlert({
+            type: "warning",
+            heading: "Registration unsuccessful.",
+            message: <div>{info.reason}</div>,
+          });
+      }
+    }
+  }
+
+  function clearForm() {
+    setFirstName("");
+    setLastName("");
+    setUsername("");
+    setOrgEmail("");
+    setPassword("");
+  }
+
   return (
     <div className="RegisterForm">
-      <form id="registerForm">
+      <form id="registerForm" onSubmit={onSubmit}>
         <FormInput
           type="text"
           idAndName="firstName"
           labelContent="First Name"
+          value={firstName}
+          onChange={(evt) => setFirstName(evt.target.value)}
         />
-        <FormInput type="text" idAndName="lastName" labelContent="Last Name" />
-        <FormInput type="text" idAndName="username" labelContent="Username" />
+        <FormInput
+          type="text"
+          idAndName="lastName"
+          labelContent="Last Name"
+          value={lastName}
+          onChange={(evt) => setLastName(evt.target.value)}
+        />
+        <FormInput
+          type="text"
+          idAndName="username"
+          labelContent="Username"
+          value={username}
+          onChange={(evt) => setUsername(evt.target.value)}
+        />
         <FormInput
           type="email"
           idAndName="orgEmail"
           labelContent="Organization Email"
+          value={orgEmail}
+          onChange={(evt) => setOrgEmail(evt.target.value)}
         />
         <FormInput
           type="password"
           idAndName="password"
           labelContent="Password"
+          value={password}
+          onChange={(evt) => setPassword(evt.target.value)}
         />
         <button type="submit" className="btn btn-primary">
           Register
@@ -37,6 +147,8 @@ function RegisterForm() {
   );
 }
 
-RegisterForm.propTypes = {};
+RegisterForm.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default RegisterForm;
--- front-end/src/pages/RegisterPage.js
+++ front-end/src/pages/RegisterPage.js
@@ -1,24 +1,36 @@
 // By Mea
 import "../stylesheets/RegisterPage.css";
+import React, { useState } from "react";
 import RegisterForm from "../components/RegisterForm";
+import Alert from "../components/base-page-components/Alert";
 
 function RegisterPage() {
+  const [alert, setAlert] = useState({
+    type: "",
+    heading: "",
+    message: <div></div>,
+  });
+
   return (
     <div className="RegisterPage">
       <div className="container-fluid">
         <div className="row">
-          <div className="col img-col">
+          <div className="col d-none d-sm-flex img-col">
             <img
               src="/media/example-images/pexels-do-castle-2158963.jpeg"
               className="img-fluid"
-              // src="/pexels-do-castle-2158963.jpeg"
-              alt="People biking"
+              alt="Three people riding mountain bikes through bright green grass and trees."
             ></img>
           </div>
           <div className="col form-col">
             <div>
+              <Alert
+                type={alert.type}
+                heading={alert.heading}
+                message={alert.message}
+              />
               <h1>Register</h1>
-              <RegisterForm />
+              <RegisterForm setAlert={setAlert} />
             </div>
           </div>
         </div>

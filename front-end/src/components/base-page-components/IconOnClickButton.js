354dd61a04562e8e93b848cf2abfece6f8661e93
m-siegel <mea.siegel@gmail.com>
2022-11-23 22:31:11 -0800

    added IconOnClickButton component

---
+++ front-end/src/components/base-page-components/IconOnClickButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function IconOnClickButton({
+  icon,
+  descriptionText,
+  onClick,
+  className,
+  inline = false,
+}) {
+  return (
+    <button
+      type="button"
+      className={`IconButton IconOnClickButton btn ${className}`}
+      onClick={onClick}
+    >
+      {icon}
+      {inline ? (
+        <span>
+          <small className="timea-text-muted">{descriptionText}</small>
+        </span>
+      ) : (
+        <p>
+          <small className="timea-text-muted">{descriptionText}</small>
+        </p>
+      )}
+    </button>
+  );
+}
+
+IconOnClickButton.propTypes = {
+  icon: PropTypes.element.isRequired,
+  descriptionText: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  inline: PropTypes.bool,
+};
+
+export default IconOnClickButton;

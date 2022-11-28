9ae3d3d16e5a682679727b4929e9343c49e0bc56
tcrawley2 <62876986+tcrawley2@users.noreply.github.com>
2022-11-28 04:28:53 -0800

    createEvent page

---
+++ front-end/src/pages/CreateEventPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import BasePage from "../components/base-page-components/BasePage.js";
+import useAlert from "../hooks/useAlert.js";
+import CreateEventForm from "../components/CreateEventForm.js";
+
+function CreateEventPage() {
+  const [AlertComponent, setAlert] = useAlert();
+
+  return (
+    <BasePage>
+      <AlertComponent />
+      <div className="card">
+        <div className="card-body">
+          <h1>Create an event</h1>
+          <CreateEventForm setAlert={setAlert} />
+        </div>
+      </div>
+    </BasePage>
+  );
+}
+
+export default CreateEventPage;

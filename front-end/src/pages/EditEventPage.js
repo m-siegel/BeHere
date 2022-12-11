3ce90c2d68178cb9f61949c6d39f56534459b4fc
tcrawley2 <62876986+tcrawley2@users.noreply.github.com>
2022-12-11 14:35:41 -0800

    Added styling to see alertcomponent a bit better

--- front-end/src/pages/EditEventPage.js
+++ front-end/src/pages/EditEventPage.js
@@ -6,6 +6,7 @@ import EventForm from "../components/EventForm.js";
 import useAlert from "../hooks/useAlert.js";
 import ConfirmDeleteComponent from "../components/ConfirmDeleteComponent.js";
 import PropTypes from "prop-types";
+import "../stylesheets/EditEventPage.css";
 
 function EditEventPage({ isAuth }) {
   const [del, setDel] = useState(false);
@@ -22,16 +23,22 @@ function EditEventPage({ isAuth }) {
   }, [isAuth, navigate]);
 
   return (
-    <BasePage>
-      <AlertComponent />
-      <div className="card">
-        <div className="card-body">
-          <h1>Edit your event</h1>
-          <EventForm setAlert={setAlert} setDel={setDel} navigate={navigate} />
+    <div className="EditEventPage">
+      <BasePage>
+        <AlertComponent />
+        <div className="card">
+          <div className="card-body">
+            <h1>Edit your event</h1>
+            <EventForm
+              setAlert={setAlert}
+              setDel={setDel}
+              navigate={navigate}
+            />
+          </div>
         </div>
-      </div>
-      <ConfirmDeleteComponent del={del} setDel={setDel} setAlert={setAlert} />
-    </BasePage>
+        <ConfirmDeleteComponent del={del} setDel={setDel} setAlert={setAlert} />
+      </BasePage>
+    </div>
   );
 }
 

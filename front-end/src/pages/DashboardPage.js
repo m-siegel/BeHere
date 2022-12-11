1ba3ae70e6826b33d7539a58d1c445fd972e614e
tcrawley2 <62876986+tcrawley2@users.noreply.github.com>
2022-12-10 17:27:37 -0800

    fixed overlap of EventPreviews

--- front-end/src/pages/DashboardPage.js
+++ front-end/src/pages/DashboardPage.js
@@ -146,7 +146,7 @@ function DashboardPage({ isAuth }) {
             Events I'm following
           </button>
         </div>
-        <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 row-cols-xxl-4">
+        <div className="row">
           {previews.length ? (
             previews.map((p, i) => (
               <div className="col" key={p._id}>
